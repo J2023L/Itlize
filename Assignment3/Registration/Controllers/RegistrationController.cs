22041e04e5ac2eb9b91adb657420cd0315eeb221
J2023L <152556170+J2023L@users.noreply.github.com>
2024-03-14 23:54:15 -0700

    Add files via upload

---
+++ Assignment3/Registration/Controllers/RegistrationController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Registration.Models;
+using Registration.DAL;
+
+namespace Registration.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        UserDAL userDAL = new UserDAL();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View("Register");
+        }
+
+
+        [HttpPost]
+        public ActionResult RegisterUser(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                userDAL.AddUser(user);
+                return RedirectToAction("UserList");
+            }
+            return View("Register", user);
+        }
+
+        [HttpGet]
+        public ActionResult UserList()
+        {
+            List<User> user = userDAL.GetUsers();
+            return View(user);
+        }
+
+        public ActionResult GetUserInfo(string UserN)
+        {
+            var userList = userDAL.GetUserInfo(UserN);
+            return View("UserList", userList);
+        }
+    }
+}
