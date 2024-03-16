0db9e4c8779f4f132db0e41bba39acfad60efdc9
J2023L <152556170+J2023L@users.noreply.github.com>
2024-03-16 00:52:11 -0700

    Add files via upload

---
+++ Assignment4/BLL_Layer/BLLclass.cs
@@ -0,0 +1,33 @@
+﻿using DAL_Layer;
+using DAL_Layer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL_Layer
+{
+    public class BLLclass
+    {
+        UserDAL userDAL = new UserDAL();
+
+        public void AddUser(User user)
+        {
+            userDAL.AddUser(user);
+        }
+
+        public List<User> GetAllUsers()
+        {
+            List<User> userAll = userDAL.GetUsers();
+            return userAll;
+        }
+
+        public List<User> GetUsers(string UserN)
+        {
+            var userList = userDAL.GetUserInfo(UserN);
+            return userList;   
+        }
+
+    }
+}
---
+++ Assignment4/DAL_Layer/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_Layer.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email Address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPW { get; set; }
+    }
+}
