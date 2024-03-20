c4df750a6d938dd47cab7da869bd53aaed2eff89
J2023L <152556170+J2023L@users.noreply.github.com>
2024-03-19 22:03:24 -0700

    Add files via upload

---
+++ Assignment4/Registration/Controllers/RegistrationController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BLL_Layer;
+using BLL_Layer.Models;
+
+namespace Registration.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        BLLclass Bll = new BLLclass();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View("Register");
+        }
+
+
+        [HttpPost]
+        public ActionResult RegisterUser(UserBLL user)
+        {
+            if (ModelState.IsValid)
+            {
+                Bll.AddUser(user);
+                return RedirectToAction("UserList");
+            }
+            return View("Register", user);
+        }
+
+        [HttpGet]
+        public ActionResult UserList() 
+        {
+            List<UserBLL> user = Bll.GetAllUsers();
+            return View(user);
+        }
+
+        public ActionResult GetUserInfo(string UserN)
+        {
+            var userList = Bll.GetUsers(UserN);
+            return View("UserList", userList);
+        }
+    }
+}
---
+++ Assignment4/Registration/Models/UserPRE.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Registration.Models
+{
+    public class UserPRE
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email Address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPW { get; set; }
+    }
+}
